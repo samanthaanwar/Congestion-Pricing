172fd1c800768e98addc600bca28191ec0a55401
samanthaanwar <soa2134@columbia.edu>
2025-03-05 23:13:45 -0500

    first commit

---
+++ Welcome.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="NYC Congestion Pricing",
+    page_icon="🚗",
+)
+
+st.write("# Preliminary Effects of Congestion Pricing in New York City 🚗")
+st.write('#### Project by Sam Anwar, Ishraq Khan, and Devraj Singhania')
+st.write('#### Data Science & Public Policy | Columbia University')
+
+st.markdown(
+    """
+    WORK IN PROGRESS
+
+    Insert executive summary / introduction here.
+    """
+)
