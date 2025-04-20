6051b29d402174edcbd901278933a6f62bddb6d4
samanthaanwar <soa2134@columbia.edu>
2025-04-20 18:10:48 +0000

    update x axis labels

--- pages/3_Commute_Speeds.py
+++ pages/3_Commute_Speeds.py
@@ -23,12 +23,11 @@ keep = ['3rd Avenue - Northbound - 49th St to 57th St',
         '23rd Street - Westbound - 6th Ave to 7th Ave',
         '34th Street - Westbound - 3rd Ave to Madison Ave']
 
-
 BASE_DIR = Path(__file__).parent.parent
 DATA_DIR = BASE_DIR / 'gtfs_m'
 
 # Load multiple files
-agg_hr_df = pd.read_csv(DATA_DIR / 'commute_speeds.csv')
+commute_speeds = pd.read_csv(DATA_DIR / 'commute_speeds.csv')
 unique_routes = pd.read_csv(DATA_DIR / 'unique_routes.csv')
 unique_routes['geometry'] = unique_routes['geometry'].apply(wkt.loads)
 unique_routes = gpd.GeoDataFrame(unique_routes, geometry='geometry', crs="EPSG:4326")
@@ -72,16 +71,42 @@ day_choice = st.selectbox(
     'Thursday', 'Friday', 'Saturday', 'Sunday')
 )
 
+commute_speeds['date'] = pd.to_datetime(commute_speeds['date'])
+commute_speeds['hour'] = commute_speeds['date'].dt.time
+commute_speeds['hour_label'] = commute_speeds['date'].apply(lambda x: x.strftime('%I:%M %p'))
+commute_speeds['weekday'] = commute_speeds['date'].dt.day_name()
+
+period = []
+for day in commute_speeds['date']:
+    if day < datetime(2024, 12, 31):
+        period.append('Pre-CP')
+    else:
+        period.append('CP in Effect')
+
+commute_speeds['period'] = period
+
+agg_hr_df = commute_speeds.groupby(['link_name', 'date', 'hour', 'hour_label', 'weekday', 'period'])['mph'].mean().reset_index()
+
 choice = agg_hr_df[(agg_hr_df.link_name == route_choice) & (agg_hr_df.weekday == day_choice)]
+
 choice = (choice
-    .groupby(['weekday', 'hour', 'hour_label', 'period', 'link_name'])['mph']
-    .mean()
-    .reset_index()
-    .sort_values(by='hour'))
+          .groupby(['weekday', 'hour', 'hour_label', 'period', 'link_name'])['mph']
+          .mean()
+          .reset_index())
+
+choice = choice.sort_values(by='hour')
 
 line_plot = px.line(choice, x = 'hour_label', y = 'mph', color = 'period', line_shape = 'spline')
 line_plot.update_layout(height = 500, width = 900, 
     yaxis_title = 'Average Speed (mph)', xaxis_title = '',
     font_family='Arial', legend_title = 'Period')
+
+# Define sorted list of labels manually
+hour_order = pd.date_range("00:00", "23:00", freq="1H").strftime("%I:%M %p").tolist()
+
+line_plot.update_xaxes(
+    categoryorder='array',
+    categoryarray=hour_order
+)
 
 st.plotly_chart(line_plot)
